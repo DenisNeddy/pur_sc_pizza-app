acd10665e429ec5ebf9bd79e1f73900b921eb2e1
Денис Жилин <zilin.d@sm.local>
2024-10-15 16:32:00 +0300

    10.7 Подготовка стилей и шрифтов

--- pizza-app/src/App.tsx
+++ pizza-app/src/App.tsx
@@ -1,15 +1,27 @@
 
-import './App.css';
+
 import Button from './components/Button/Button';
+import { MouseEvent, useState } from 'react';
 
 function App() {
+  
+	const [counter, setCounter] = useState<number>(0);
+	console.log(counter);
+
+	const addCounter = (event: MouseEvent) => {
+		setCounter(2);
+    
+
+		console.log(event);
+
+	};
 
 
-  return (
-    <>
-      <Button onClick={() => console.log('Хуета')}>Кнопочка</Button>
-    </>
-  )
+	return (
+		<>
+			<Button onClick={addCounter}>Кнопочка</Button>
+		</>
+	);
 }
 
-export default App
+export default App;
