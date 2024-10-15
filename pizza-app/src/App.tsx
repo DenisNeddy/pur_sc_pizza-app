1a869ec5741bac8c80f599b1ecb799d9a7b7e9d7
Денис Жилин <zilin.d@sm.local>
2024-10-15 17:53:05 +0300

    11.2 Создание Роутов

--- pizza-app/src/App.tsx
+++ pizza-app/src/App.tsx
@@ -3,6 +3,10 @@
 import Button from './components/Button/Button';
 import { MouseEvent, useState } from 'react';
 import Input from './components/Input/Input';
+import { Routes, Route } from 'react-router-dom';
+import Menu from './pages/Menu/Menu';
+import Cart from './pages/Cart/Cart';
+import Error from './pages/Error/Error';
 
 function App() {
   
@@ -23,6 +27,15 @@ function App() {
 			<Button onClick={addCounter}>Кнопочка</Button>
 			<Button onClick={addCounter} appearence='big'>Кнопочка 2</Button>
 			<Input placeholder='Email'/>
+			<div>
+				<a href='/'>Меню</a>
+				<a href='/cart'>Козина</a>
+			</div>
+			<Routes>
+				<Route  path='/' element={<Menu />} />
+				<Route  path='/cart' element={<Cart />} />
+				<Route  path='*' element={<Error />} />
+			</Routes>
 		</>
 	);
 }
---
+++ pizza-app/src/pages/Cart/Cart.tsx
@@ -0,0 +1,11 @@
+
+
+const Cart = () => {
+	return (
+		<div>
+            Корзина
+		</div>
+	);
+};
+
+export default Cart;
