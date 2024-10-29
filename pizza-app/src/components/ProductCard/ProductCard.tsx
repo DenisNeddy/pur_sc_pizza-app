f7ba386237b246a2c067b43af05fceef6f147837
Денис Жилин <zilin.d@sm.local>
2024-10-29 17:54:13 +0300

    11.10 Карточка товара

---
+++ pizza-app/src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,31 @@
+import styles from './ProductCard.module.css';
+import { ProductCardProps } from './ProductCard.props';
+import { Link } from 'react-router-dom';
+
+const ProductCard = (props:ProductCardProps) => {
+	return (
+		<Link to={'/.vscode/'} className={styles['link']}>
+			<div className={styles['card']}>
+				<div className={styles['head']} style={{backgroundImage: `url('${props.image}')`}}>
+					<div className={styles['price']}>
+						{props.price} &nbsp;
+						<span className={styles['currency']}>₽</span>
+					</div>
+					<button className={styles['add-to-cart']}>
+						<img src='/cart_icon_white.svg' alt="Кнопка добавления товара в карзину" />
+					</button>
+					<div className={styles['rating']}>
+						{props.rating} &nbsp;
+						<img src="/star_icon.svg" alt="Иконка звезда рейтинга" />
+					</div>
+				</div>
+				<div className={styles['footer']}>
+					<p className={styles['title']}>{props.title}</p>
+					<p className={styles['description']}>{props.description}</p>
+				</div>
+			</div>
+		</Link>
+	);
+};
+
+export default ProductCard;
--- pizza-app/src/pages/Menu/Menu.tsx
+++ pizza-app/src/pages/Menu/Menu.tsx
@@ -1,14 +1,28 @@
 import Headling from '../../components/Headling/Headling';
 import Search from '../../components/Search/Search';
 import styles from './Menu.module.css';
+import ProductCard from '../../components/ProductCard/ProductCard';
 
 const Menu = () => {
 	return (
+		<>
+			<div className={styles['head']}>
+				<Headling>Меню</Headling>
+				<Search placeholder='Введите блюдо или состав'/>
+			</div>
+			<div>
+				<ProductCard 
 
-		<div className={styles['head']}>
-			<Headling>Меню</Headling>
-			<Search placeholder='Введите блюдо или состав'/>
-		</div>
+					id={1}
+					title='Наслаждение'
+					description='Салями, руккола, помидоры, оливки'
+					rating={4.5}
+					price={300}
+					image='/cart_img_1.jpg'
+				
+				/>
+			</div>
+		</>
 	
 	);
 };
